563e46f996e7598508f578b430e58b1d4496b635
iotserver24 <iotserver24@gmail.com>
2025-05-23 23:01:59 +0530

    final changes

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let menu_icon = document.querySelector('#menu-icon');
+let menu = document.querySelector('.menu');
+
+menu_icon.onclick = () => {
+    menu_icon.classList.toggle('bx-x');
+    menu.classList.toggle('open');
+}
+
+// Three.js Animation
+const container = document.getElementById('threejs-container');
+if (container) {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  container.appendChild(renderer.domElement);
+
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  }
+  animate();
+}
---
+++ js/threejs-react.js
@@ -0,0 +1,38 @@
+// threejs-react.js
+const ThreeJSAnimation = () => {
+  const containerRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (!containerRef.current) return;
+
+    const container = containerRef.current;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    container.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    function animate() {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    }
+    animate();
+
+    return () => {
+      container.removeChild(renderer.domElement);
+    };
+  }, []);
+
+  return <div ref={containerRef} style={{ width: '100%', height: '400px' }} />;
+};
+
+ReactDOM.render(<ThreeJSAnimation />, document.getElementById('threejs-container')); 
